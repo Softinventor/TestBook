fb83c9b80a2838a37c92e1ce1d4f65977d8fcb4c
Martin Fog Lihme <martin.lihme@softinventor.dk>
2017-08-09 12:10:02 +0200

    change2 test

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("You are so beauifull");
+            Console.WriteLine("You are so beauifull Change 2");
         }
     }
 }
